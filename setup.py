a2ae24f074b718a180aabbe4005b667aa5163461
JAIME ANDRES POLANCO JIMENEZ <49122673+JAPJ182@users.noreply.github.com>
2022-03-19 18:51:47 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import setup
- 
+import os
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
 setup(
     # Needed to silence warnings (and to be a worthwhile package)
     name='GCP_data_eng_functions',
@@ -9,18 +11,19 @@ setup(
     # Needed to actually package something
     packages=['gdp_dataeng_functions'],
     # Needed for dependencies
-    install_requires=['chardet==4.0.0',
-                        'office365==0.3.15',
-                        'Office365_REST_Python_Client', #==2.2.1,
-                        'openpyxl==3.0.9',
-                        'pandas==1.3.1',
-                        'protobuf==3.19.4',
-                        'pymongo==4.0.1',
-                        'pytz==2021.1',
-                        'requests==2.25.1',
-                        'sendgrid==6.9.6',
-                        'tqdm==4.61.2',
-                        'patool==1.12' , 'numpy' ],
+    install_requires=required,
+#     install_requires=['chardet==4.0.0',
+#                         'office365==0.3.15',
+#                         'Office365_REST_Python_Client', #==2.2.1,
+#                         'openpyxl==3.0.9',
+#                         'pandas==1.3.1',
+#                         'protobuf==3.19.4',
+#                         'pymongo==4.0.1',
+#                         'pytz==2021.1',
+#                         'requests==2.25.1',
+#                         'sendgrid==6.9.6',
+#                         'tqdm==4.61.2',
+#                         'patool==1.12' , 'numpy' ],
     # *strongly* suggested for sharing
     version='0.1',
     # The license can be anything you like
