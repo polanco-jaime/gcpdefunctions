fc6d6692655258f5a906530fbb0104a942efd6fb
JAIME ANDRES POLANCO JIMENEZ <49122673+JAPJ182@users.noreply.github.com>
2022-03-19 21:45:01 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,27 +1,3 @@
-# from setuptools import setup
-# import os
-# with open('requirements.txt') as f:
-#     required = f.read().splitlines()
-# setup(
-#     # Needed to silence warnings (and to be a worthwhile package)
-#     name='GCP_data_eng_functions',
-#     url= 'https://github.com/JAPJ182/GCP_data_eng_functions',
-#     author='Jaime Polanco Ladan',
-#     author_email='jaime.polanco@javeriana.edu.co',
-#     # Needed to actually package something
-#     packages=['gdp_dataeng_functions'],
-#     # Needed for dependencies
-#     install_requires=required,
- 
-#     # *strongly* suggested for sharing
-#     version='0.1',
-#     # The license can be anything you like
-#     license='Jaime Polanco Development',
-#     description='Ways for downloading the last file loaded into a specific bucket and way for loading this data into bigquery',
-#     # We will also need a readme eventually (there will be a warning)
-#     # long_description=open('README.txt').read(),
-# )
-
 from setuptools import setup, find_packages
 from codecs import open
 from os import path
@@ -47,7 +23,8 @@ with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
 setup(name='GCP_data_eng_functions',
       version='0.1.0',
       license='Jaime Polanco Development',
-      packages=find_packages("gdp_dataeng_functions", exclude=["test"]) ,
+      package_dir={'':'gdp_dataeng_functions'},
+      packages=find_packages("gdp_dataeng_functions", exclude=["*.test", "*.test.*", "test.*", "test"]) ,
       description='Ways for downloading the last file loaded into a specific bucket and way for loading this data into bigquery',
       url='https://github.com/JAPJ182/GCP_data_eng_functions',
       author='Jaime Polanco',
