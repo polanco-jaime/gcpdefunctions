7d8f568e0da143e9cfcd90604e29bfa355feb966
JAIME ANDRES POLANCO JIMENEZ <49122673+JAPJ182@users.noreply.github.com>
2022-03-19 21:51:41 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
 setup(name='GCP_data_eng_functions',
       version='0.1.0',
       license='Jaime Polanco Development',
-      package_dir={'':'gdp_dataeng_functions'},
+      #package_dir={'':'gdp_dataeng_functions'},
       packages=find_packages("gdp_dataeng_functions", exclude=["*.test", "*.test.*", "test.*", "test"]) ,
       description='Ways for downloading the last file loaded into a specific bucket and way for loading this data into bigquery',
       url='https://github.com/JAPJ182/GCP_data_eng_functions',
