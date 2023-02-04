eb82e47005524e7c22201cc988b038a1efa9ca48
JAIME ANDRES POLANCO JIMENEZ <49122673+JAPJ182@users.noreply.github.com>
2023-02-04 16:39:23 -0300

    to parquet

--- other_function/from_shp_to_csv_in_gs.py
+++ other_function/from_shp_to_csv_in_gs.py
@@ -23,15 +23,15 @@ import time
 class from_shp_to_csv_in_gs:
   def __init__(self, file_name = '' , project_id = None, bucket = None):
     self.file_name = file_name
-    self.name_in_gs = file_name.split('/')[-1].split('.')[0] + '.csv'
+    self.name_in_gs = file_name.split('/')[-1].split('.')[0] + '.parquet'
     self.buket_gs = bucket
   def reading_shp(self):
     shp_file = geopandas.read_file(self.file_name)
     shp_file = shp_file.to_crs(4326)
     shp_file['centroid']  = shp_file.centroid
     shp_file=shp_file.applymap(str )
-    
-    shp_file.to_csv( self.name_in_gs , sep = "|")
+    shp_file.to_parquet( self.name_in_gs )
+#     shp_file.to_csv( self.name_in_gs , sep = "|")
     if project_id != None:
       from google.colab import auth
       auth.authenticate_user()
